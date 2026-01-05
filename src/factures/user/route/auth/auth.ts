4cc999232e25250eefaa35216862a2c9a177d974
david123456858 <juandavidperaltafuentes@gmail.com>
2026-01-05 17:53:45 -0500

    Listo ya tenemos login

--- src/factures/user/route/auth/auth.ts
+++ src/factures/user/route/auth/auth.ts
@@ -1,17 +1,15 @@
-import { Router } from 'express'
-import { route } from '../../../../config/route/route'
-import { ServiceAuthLogin } from '../../service/auth/login'
-import { AuthController } from '../../controller/auth/auth'
+import { Router } from "express";
+import { route } from "../../../../config/route/route";
+import { ServiceAuthLogin } from "../../service/auth/login";
+import { AuthController } from "../../controller/auth/auth";
 
 export const routeAuth = (prefix: string): Router => {
-  const service = new ServiceAuthLogin()
-  const controllerAuthLogin = new AuthController(service)
+  const service = new ServiceAuthLogin();
+  const controllerAuthLogin = new AuthController(service);
 
-  route.post(`${prefix}/login`, controllerAuthLogin.login)
+  route.post(`${prefix}/login`, controllerAuthLogin.login);
 
-  route.get(`${prefix}/login`, controllerAuthLogin.login)
+  route.post(`${prefix}/register`, () => ({}));
 
-  route.post(`${prefix}/register`, () => ({}))
-
-  return route
-}
+  return route;
+};
