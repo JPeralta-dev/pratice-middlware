e0f622408ce7bf586a7ee933500827a0cff0ba6d
david123456858 <juandavidperaltafuentes@gmail.com>
2026-02-07 22:43:46 -0500

    agregando zod

--- src/config/env/env.ts
+++ src/config/env/env.ts
@@ -11,6 +11,12 @@ const envSchema = z.object({
 
 let env: z.infer<typeof envSchema>;
 
+/**
+ * Esto es typeScripts un poco duro pero explico para acordarme y enteder mejor
+ * como el parse() cuando da error explota, necesito envolverlo en un try para copntrolarlo,
+ * ahora, lo que le indicamos es con el z.infer, es mira a la variable envSchema y saca los tipos exactos,
+ * el ya entiende que env es ahora de tipo loq ue corresponde envSchema y
+ */
 try {
   env = envSchema.parse(process.env);
   console.log("Se cargaron correctamente");
